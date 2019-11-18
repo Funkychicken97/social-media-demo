ebea59695442757531b54decc1116fdae1afa171
Samuel Lovitt <samuellovitt@Samuels-MBP.hsd1.ms.comcast.net>
2019-11-17 22:10:55 -0600

    nil class error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'tag_tweets' => 'epicenter#tag_tweets'
   root 'epicenter#feed'
+  #####################
+  get 'tag_tweets' => 'epicenter#tag_tweets'
+  get 'all_users' => 'epicenter#all_users'
   get 'show_user' => 'epicenter#show_user'
   get 'now_following' => 'epicenter#now_following'
   get 'unfollow' => 'epicenter#unfollow'
+  get 'following' => 'epicenter#following'
+  get 'followers' => 'epicenter#followers'
   resources :tweets
   devise_for :users
+  #####################
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
